0439c294c92da013f18e791a3105b27df7305cae
Ilyescmoi <ilyes.mechouar@epitech.eu>
2024-12-06 11:40:56 +0100

    feat(init): init training react project

---
+++ bank_app_react/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+        <h1 className="text-amber-50">Hello World!</h1>
+    </div>
+);
+
+export default App;
