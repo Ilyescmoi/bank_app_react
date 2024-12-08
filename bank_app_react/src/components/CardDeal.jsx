fc52cdc3617e9687b826980b92b5607cf6d5d527
Ilyescmoi <ilyes.mechouar@epitech.eu>
2024-12-08 02:20:29 +0100

    feat: billing and card deal

--- bank_app_react/src/components/CardDeal.jsx
+++ bank_app_react/src/components/CardDeal.jsx
@@ -1,9 +1,27 @@
-﻿import React from "react";
-import Testimonials from "./Testimonials.jsx";
-export const CardDeal = () => {
-    return (
-        <></>
-    )
-}
+﻿import styles, {layout} from "../style.js";
+import {apple, card, google} from "../assets/index.js";
+import Button from "./Button.jsx";
+
+export const CardDeal = () => (
+    <section id="product" className={layout.sectionReverse}>
+        <div className={layout.sectionInfo}>
+            <h2 className={styles.heading2}>
+                Find a better card deal <br className="sm:block hidden"/> in few easy
+                steps.
+            </h2>
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+                aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+            </p>
+
+            <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+                <Button styles={'mt-10'}/>
+            </div>
+        </div>
+        <div className={layout.sectionImgReverse}>
+            <img src={card} alt="bill" className="w-[100%] h-[100%]"/>
+        </div>
+    </section>
+);
 
 export default CardDeal;
